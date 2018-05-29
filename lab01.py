991879ed786d68136421ea4f8b7d0889cee6154a
Ellie Beck <beck.ellie@gmail.com>
2018-05-29 16:50:26 -0400

    changed spacing on line 3

--- lab01.py
+++ lab01.py
@@ -1,4 +1,4 @@
 integer_1 = int(input("Enter one integer: "))
 integer_2 = int(input("Enter one integer: "))
 integer_3 = int(input("Enter one more integer: "))
-print("You put in: ", integer_1, integer_2, integer_3)
+print("You put in:", integer_1, integer_2, integer_3)
